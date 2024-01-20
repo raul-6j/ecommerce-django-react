8dbd58095a2a4560ac1969706e6b61f155e0861d
Rahul Raj <raul.6j@gmail.com>
2024-01-21 00:56:32 +0530

    Rating Component added and React Router Implemented

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,29 +1,31 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import Rating from "./Rating";
+import { Link } from 'react-router-dom'
 
 function Product({ product }) {
   return (
     <Card className="my-3 p-3 rounded">
-      <a href={"/product/${product._id}"}>
-        <Card.Img src={product.image} />
-      </a>
+      <Link to={`/product/${product._id}`}>
+        {/* Use backticks for the template literal */}
+        <Card.Img src={product.image} alt={product.name} />
+      </Link>
 
       <Card.Body>
-        <a href={"/product/${product._id}"}>
+        <Link to={`/product/${product._id}`}>
+          {/* Use backticks for the template literal */}
           <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
 
         <Card.Text as="div">
           <div className="my-3">
-            {product.rating} from {product.numReviews} reviews
+            <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'} />
           </div>
         </Card.Text>
 
-        <Card.Text as="h3">
-            ${product.price}
-        </Card.Text>
+        <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
     </Card>
   );
