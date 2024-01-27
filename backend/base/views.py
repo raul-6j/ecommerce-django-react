609ba0fd731f8d622e1a853edc8e26c360008399
Rahul Raj M A <raul.6j@gmail.com>
2024-01-28 02:53:28 +0530

    Connected frontend to the database

--- backend/base/views.py
+++ backend/base/views.py
@@ -19,10 +19,7 @@ def getProducts(request):
 
 @api_view(['GET'])
 def getProduct(request, pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
 
-    return Response(product)
+    return Response(serializer.data)
