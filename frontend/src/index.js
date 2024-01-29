92fff30c5fedc974da0e4918c2ca81109f15673a
Rahul Raj M A <raul.6j@gmail.com>
2024-01-30 02:07:29 +0530

    Created Redux Store, Product List Reducer and Action and Bring Reduc to Home Screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import './bootstrap.min.css'
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 
+import "./index.css";
+import "./bootstrap.min.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-
-
-
