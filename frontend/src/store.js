1ff219caaefb6201aa7831703989bc11c893eef3
Rahul Raj <raul.6j@gmail.com>
2024-02-28 01:17:30 +0530

    User Login and Register Sections Added (Reducer,Action,Screen)

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,21 +4,34 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { productListReducers, productDetailsReducer } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducer, userRegisterReducer } from './reducers/userReducers';
 
 
 const reducer = combineReducers({
     productList: productListReducers,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    userLogin : userLoginReducer,
+    userRegister : userRegisterReducer,
 });
 
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ?
     JSON.parse(localStorage.getItem('cartItems')) : []
 
+const userInfoFromStorage = localStorage.getItem('userInfo') ?
+    JSON.parse(localStorage.getItem('userInfo')) : null
+
+
+
+
 const initialState = {
     cart: {
-        cartItems: cartItemsFromStorage}
+        cartItems: cartItemsFromStorage},
+    
+     userLogin: { 
+        userInfo: userInfoFromStorage },
+    
 };
 
 const middleware = [thunk];  // Define the middleware array
