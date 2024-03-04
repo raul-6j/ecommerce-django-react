16ea69267740769dda8822ac9740148f41c93adb
Rahul Raj <raul.6j@gmail.com>
2024-03-05 01:12:28 +0530

    Profile Screen and Update User Profile Added

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,7 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { productListReducers, productDetailsReducer } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers'
-import { userLoginReducer, userRegisterReducer } from './reducers/userReducers';
+import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer } from './reducers/userReducers';
 
 
 const reducer = combineReducers({
@@ -13,6 +13,8 @@ const reducer = combineReducers({
     cart: cartReducer,
     userLogin : userLoginReducer,
     userRegister : userRegisterReducer,
+    userDetails : userDetailsReducer,
+    userUpdateProfile : userUpdateProfileReducer,
 });
 
 
